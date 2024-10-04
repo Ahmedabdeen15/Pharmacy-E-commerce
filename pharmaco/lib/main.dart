7893796975378e98c08b016567349b3373ff7192
Ziad Nasser <ziad78788@gmail.com>
2024-10-04 23:56:35 +0300

    edit password widget and add text input widget

--- pharmaco/lib/main.dart
+++ pharmaco/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pharmaco/widget/password_widget.dart';
+import 'package:pharmaco/widget/text_input_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(),
+      home:  Scaffold(),
     );
   }
 }
--- pharmaco/lib/widget/password_widget.dart
+++ pharmaco/lib/widget/password_widget.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:pharmaco/app_colors.dart';
 
-class PasswordWidget extends StatelessWidget {
+class PasswordWidget extends StatefulWidget {
   const PasswordWidget({super.key});
 
   @override
+  State<PasswordWidget> createState() => _PasswordWidgetState();
+}
+
+class _PasswordWidgetState extends State<PasswordWidget> {
+
+      bool hidePassword = true;
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: 
-        
          SizedBox(
-          
-
                 width: MediaQuery.of(context).size.width * .7,
-
-              
                 child: Container(
-                  decoration: BoxDecoration(color: AppColors.white),
+                  decoration: const BoxDecoration(color: Color.fromARGB(255, 255, 255, 255)),
                   child: TextFormField(
                     validator: (password) {
                       if (password!.length < 8) {
@@ -24,13 +27,18 @@ class PasswordWidget extends StatelessWidget {
                       }
                       return null;
                     },
-                    obscureText: true,
+                    obscureText: hidePassword,
                     decoration: InputDecoration(
+                      suffixIcon: IconButton(onPressed: (){
+                        setState(() {
+                          hidePassword = !hidePassword;
+                        });
+                      }, icon: hidePassword ? Icon(Icons.visibility_off): Icon(Icons.visibility)),
                         border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10)),
+                            borderRadius: BorderRadius.circular(20)),
                         hintText: 'password'),
                         style: TextStyle(
-                          foreground: Paint()..shader = AppColors.primaryColor.createShader(Rect.fromLTWH(0, 0, 300, 70))
+                          foreground: Paint()..shader = AppColors.primaryColor.createShader(const Rect.fromLTWH(0, 0, 300, 70))
                         ),
                   ),
                 ),
