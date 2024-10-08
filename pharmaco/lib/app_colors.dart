6842c4805ca3985580e4a0670af1fbb500739a49
Ahemd Abdeen <ahmedwalleed15@gmail.com>
2024-10-08 17:57:35 +0300

    fix Gradient

--- pharmaco/lib/app_colors.dart
+++ pharmaco/lib/app_colors.dart
@@ -1,14 +1,15 @@
-
 import 'package:flutter/material.dart';
 
 class AppColors {
-   static const LinearGradient primaryColor =  LinearGradient(
-  colors: [Color(0xFF33E4DB), Color(0xFF00BBD3)], // End color], // Define the gradient colors
-  begin: Alignment.topLeft, // Starting point of the gradient
-  end: Alignment.bottomRight, // Ending point of the gradient
-);
- 
+  static const LinearGradient primaryColor = LinearGradient(
+    colors: [
+      Color.fromARGB(255, 51, 228, 219),
+      Color.fromARGB(255, 0, 186, 211),
+    ], // End color], // Define the gradient colors
+    begin: Alignment.topLeft, // Starting point of the gradient
+    end: Alignment.bottomRight, // Ending point of the gradient
+  );
+
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
-
 }
