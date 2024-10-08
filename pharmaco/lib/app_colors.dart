7c84d738edbe74c4361c91d624b54f9b50f52d12
Ahemd Abdeen <ahmedwalleed15@gmail.com>
2024-10-08 18:38:14 +0300

    fix color gradient

--- pharmaco/lib/app_colors.dart
+++ pharmaco/lib/app_colors.dart
@@ -6,8 +6,8 @@ class AppColors {
       Color.fromARGB(255, 51, 228, 219),
       Color.fromARGB(255, 0, 186, 211),
     ], // End color], // Define the gradient colors
-    begin: Alignment.topLeft, // Starting point of the gradient
-    end: Alignment.bottomRight, // Ending point of the gradient
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter, // Ending point of the gradient
   );
 
   static const Color white = Color(0xFFFFFFFF);
