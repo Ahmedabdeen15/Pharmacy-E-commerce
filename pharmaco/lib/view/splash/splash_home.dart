dfe6ff2647569743dc55721d31e1c0b89e9ac6f1
mod664h@gmail.com <mod664h@gmail.com>
2024-10-11 02:26:51 +0300

    Main Screen and bottom nav bar

--- pharmaco/lib/view/splash/splash_home.dart
+++ pharmaco/lib/view/splash/splash_home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:pharmaco/app_colors.dart';
+import 'package:pharmaco/view/screen/main_screen.dart';
 
 class SplashHome extends StatelessWidget {
   const SplashHome({super.key});
@@ -9,23 +10,25 @@ class SplashHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
-      backgroundColor: AppColors.primaryColor.colors.first,
+        backgroundColor: AppColors.primaryColor.colors.first,
         splashIconSize: 300,
         pageTransitionType: PageTransitionType.bottomToTop,
-        animationDuration: Duration(seconds: 3),
+        animationDuration: const Duration(seconds: 3),
         curve: Curves.bounceIn,
         centered: true,
         duration: 4000,
         splashTransition: SplashTransition.rotationTransition,
-        splash:
-           Column(children: [ 
+        splash: Column(children: [
           Image.asset("assets/logo.png"),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Text("Pharmaco",style: TextStyle(fontSize: 30,color: AppColors.white,fontWeight: FontWeight.bold)),
+          const Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Text("Pharmaco",
+                style: TextStyle(
+                    fontSize: 30,
+                    color: AppColors.white,
+                    fontWeight: FontWeight.bold)),
           ),
-          ]),
-        nextScreen: Scaffold());
+        ]),
+        nextScreen: const MainScreen());
   }
 }
-
