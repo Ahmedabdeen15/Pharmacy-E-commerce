83fb44809eb10019e20a35a24b12c0f3cd24b546
Ahemd Abdeen <ahmedwalleed15@gmail.com>
2024-10-18 22:34:38 +0300

    init cart screen

--- pharmaco/lib/view/screen/cart_screen.dart
+++ pharmaco/lib/view/screen/cart_screen.dart
@@ -1,12 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:pharmaco/data/model/cart.dart';
+import 'package:pharmaco/view/widget/cart_card.dart';
+import 'package:pharmaco/view/widget/custom_app_bar.dart';
+import 'package:pharmaco/view/widget/generic_flexible_button.dart';
 
-class CartScreen extends StatelessWidget {
-  const CartScreen({super.key});
+class CartScreen extends StatefulWidget {
+  const CartScreen({
+    super.key,
+    required this.localCart,
+  });
+  final Cart localCart;
 
   @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Cart Screen'),
+    return Scaffold(
+      appBar: const CustomAppBarr(title: 'Cart'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsetsDirectional.all(20),
+              child: ListView.builder(
+                itemCount: widget.localCart.products.length,
+                itemBuilder: (context, index) {
+                  var temp = widget.localCart.products.entries.elementAt(index);
+                  return CartCard(
+                    model: temp.value.product,
+                    count: temp.value.quantity,
+                    increment: () {},
+                    decrement: () {},
+                  );
+                },
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "Total: \$${widget.localCart.totalPrices}",
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              GenericFlexibleButton(
+                  text: "Buy Now",
+                  minWidth: 155,
+                  minHeight: 41,
+                  fontSize: 20,
+                  onPressed: () {})
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          )
+        ],
+      ),
     );
   }
 }
