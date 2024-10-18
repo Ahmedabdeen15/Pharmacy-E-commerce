292b53a71379e46a03d70ec9a237c3d3e960bf74
Ahemd Abdeen <ahmedwalleed15@gmail.com>
2024-10-18 22:33:37 +0300

    init cart screen

--- pharmaco/lib/data/model/cart.dart
+++ pharmaco/lib/data/model/cart.dart
@@ -1,28 +1,49 @@
+import 'package:pharmaco/data/model/cart_item.dart';
 import 'package:pharmaco/data/model/product.dart';
 
 class Cart {
   int id;
   bool isFinished = false;
-  Map<Product, int> products = {};
+  Map<String, CartItem> products = {};
+  double totalPrices = 0;
 
   Cart({
     this.id = 0,
   });
   void addToList(Product product, int quantity) {
-    if (product.quantity == null) {
-      products[product] = (products[product] ?? 0) + quantity;
-    } else if (product.quantity! >= quantity) {
-      products[product] = (products[product] ?? 0) + quantity;
+    String productName = product.productName ?? 'Unknown';
+    if (product.quantity == null || product.quantity! >= quantity) {
+      // Check if the product already exists in the cart by its name
+      if (products.containsKey(productName)) {
+        // Update existing quantity
+        CartItem existingItem = products[productName]!;
+        totalPrices -= existingItem.totalPrice; // Subtract old total
+        existingItem.quantity += quantity;
+        totalPrices += existingItem.totalPrice; // Add new total
+      } else {
+        // Add new product to the cart
+        CartItem newItem = CartItem(product: product, quantity: quantity);
+        products[productName] = newItem;
+        totalPrices += newItem.totalPrice;
+      }
     }
   }
 
   void removeFromList(Product product, int quantity) {
-    if (products.containsKey(product)) {
-      int currentQuantity = products[product] ?? 0;
-      if (currentQuantity > quantity) {
-        products[product] = currentQuantity - quantity;
+    String productName =
+        product.productName ?? 'Unknown'; // Use product name as key
+
+    if (products.containsKey(productName)) {
+      CartItem currentItem = products[productName]!;
+
+      totalPrices -= currentItem.totalPrice; // Subtract old total
+
+      // Adjust the quantity or remove the product from the cart
+      if (currentItem.quantity > quantity) {
+        currentItem.quantity -= quantity;
+        totalPrices += currentItem.totalPrice; // Add updated total
       } else {
-        products.remove(product);
+        products.remove(productName);
       }
     }
   }
